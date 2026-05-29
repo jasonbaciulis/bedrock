d14a5addc590e5abebf3865d0e49ab6b9942a00d
Jason Baciulis <jason@remarkable.dev>
2026-05-29 17:31:17 +0700

    feat: drop precognition

--- export/resources/js/site.js
+++ export/resources/js/site.js
@@ -2,13 +2,13 @@ import Alpine from 'alpinejs'
 import Collapse from '@alpinejs/collapse'
 import Persist from '@alpinejs/persist'
 import Focus from '@alpinejs/focus'
+import Form from './plugins/form'
 import Intersect from '@alpinejs/intersect'
 import Mask from '@alpinejs/mask'
-import Precognition from 'laravel-precognition-alpine'
 import Cookies from 'js-cookie'
 
 window.Cookies = Cookies
 window.Alpine = Alpine
 
-Alpine.plugin([Collapse, Focus, Persist, Intersect, Mask, Precognition])
+Alpine.plugin([Collapse, Focus, Persist, Intersect, Mask, Form])
 Alpine.start()
